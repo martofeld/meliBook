5fb57e8b68cb8f6dd7d7c6ca1752790e92795f20
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-20 12:09:03 -0300

    fixed error on user class

--- grails-app/domain/melibook/User.groovy
+++ grails-app/domain/melibook/User.groovy
@@ -9,7 +9,7 @@ class User {
 	Date birth
 	
 	static belongsTo = [area: Area]
-	static hasMany = [posts: Post, conversations: Conversation]
+	static hasMany = [posts: Post]
 
     static constraints = {
     	name nullable: false
