ed8bc0ffdd92c426657d82fb42235f129c2123b1
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-26 11:32:00 -0300

    changed bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import melibook.*
 class BootStrap {
 
     def init = { servletContext ->
-        Role.findByAutohority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(flush: true)
+        Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(flush: true)
     }
     def destroy = {
     }
