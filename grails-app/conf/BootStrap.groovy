ead7e05b3c48b5192cea37df1ff402b0c27be17e
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-27 09:19:50 -0300

    finished area posting :satisfied:

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,7 +4,10 @@ class BootStrap {
 
     def init = { servletContext ->
         Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(flush: true)
-        new Area(name: "it").save(flush: true)
+        new Area(name: "IT").save(flush: true)
+        new Area(name: "RRHH").save(flush: true)
+        new Area(name: "Mercado Pago").save(flush: true)
+        new Area(name: "all").save(flush: true)
     }
     def destroy = {
     }
