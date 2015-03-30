e724540704ce32907fc3e519857c7bbe1e43ca6f
Diego Sebastian Garcia <diego.garcia@mercadolibre.com>
2015-03-30 14:48:23 -0300

    fixing things

--- grails-app/controllers/melibook/UserController.groovy
+++ grails-app/controllers/melibook/UserController.groovy
@@ -12,8 +12,6 @@ class UserController {
     private static final okcontents = ['image/png', 'image/jpeg', 'image/gif']
 
     def index(String username) {
-        if (username == null)
-            username = params.username
         def user = SpringUser.findByUsername(username)?.user
         [posts: user.posts.sort{it.timestamp}.reverse(), user: user]
     }
