1bd38f091b81d0f2664587ce32a3f708dcb90ac0
Martin Feldsztejn <mfeldsztejn@gmail.com>
2015-03-30 16:46:53 -0300

    fixed null when no users on db to show on new created

--- grails-app/controllers/melibook/IndexController.groovy
+++ grails-app/controllers/melibook/IndexController.groovy
@@ -8,13 +8,26 @@ class IndexController {
 
     def index() {
     	def area = springSecurityService.currentUser.user.area
+        def lastUsers = getLastUsers();
         def result = [posts: Area.findByName("all").posts.sort { 
             it.timestamp
         }.reverse(),
         areaPosts: Area.findByName(area.name).posts.sort { 
             it.timestamp
         }.reverse(), user: springSecurityService.currentUser.user, username: springSecurityService.currentUser.username,
-         users: User.list()[User.list().size()-1..User.list().size()-3]]
+         users: lastUsers]
 
+    }
+
+    def getLastUsers(){
+        def all = User.list();
+        def lastThree = []
+
+        for(int i = 0; i < 3 && i < all.size(); i++) {
+            def last = all.getAt((-all?.size()+1+i))
+            lastThree.add(last)
+        }
+
+        return lastThree;
     }
 }
